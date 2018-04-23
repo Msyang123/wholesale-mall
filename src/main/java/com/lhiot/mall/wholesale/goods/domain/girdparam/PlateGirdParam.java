e5c928f8bcc37cabc88b43850055a6bba41a9848
Administrator <Administrator@PC-20180315HULB>
2018-04-23 18:30:51 +0800

    商品版块分类判断是否可以被删除

---
+++ src/main/java/com/lhiot/mall/wholesale/goods/domain/girdparam/PlateGirdParam.java
@@ -0,0 +1,20 @@
+package com.lhiot.mall.wholesale.goods.domain.girdparam;
+
+import com.leon.microx.common.wrapper.PageObject;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@ApiModel(description = "商品版块分页查询的参数")
+public class PlateGirdParam extends PageObject {
+	
+	@ApiModelProperty(notes="版块名称",dataType="String")
+	private String plateName;
+	
+	@ApiModelProperty(notes="分页查询起始页",dataType="Integer")
+	private Integer start;
+}
