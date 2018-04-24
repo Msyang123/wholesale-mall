d1d73d5c1ffe46dc5e8da6a639a603c309451d66
leon <leon_dev@aliyun.com>
2018-04-24 19:00:34 +0800

    初始导入

--- src/main/java/com/lhiot/mall/wholesale/user/service/UserService.java
+++ src/main/java/com/lhiot/mall/wholesale/user/service/UserService.java
@@ -1,6 +1,10 @@
 package com.lhiot.mall.wholesale.user.service;
 
+import com.leon.microx.common.exception.ServiceException;
+import com.leon.microx.util.BeanUtils;
 import com.leon.microx.util.SnowflakeId;
+import com.lhiot.mall.wholesale.user.domain.SalesUser;
+import com.lhiot.mall.wholesale.user.domain.SalesUserRelation;
 import com.lhiot.mall.wholesale.user.domain.User;
 import com.lhiot.mall.wholesale.user.domain.UserAddress;
 import com.lhiot.mall.wholesale.user.mapper.UserMapper;
@@ -9,22 +13,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @Transactional
 public class UserService {
 
-    private final SnowflakeId snowflakeId;
-
     private final UserMapper userMapper;
-
+    private final SalesUserService salesUserService;
 
     @Autowired
-    public UserService(SnowflakeId snowflakeId, UserMapper userMapper) {
-        this.snowflakeId = snowflakeId;
+    public UserService(UserMapper userMapper, SalesUserService salesUserService) {
         this.userMapper = userMapper;
+        this.salesUserService = salesUserService;
     }
-
 
     public List<User> search(List ids) {
         return userMapper.search(ids);
@@ -34,40 +36,54 @@ public class UserService {
         return userMapper.updateUserStatus(id);
     }
 
-    public User user(long id){
+    public User user(long id) {
         return userMapper.user(id);
     }
 
-    public boolean updateUser(User user){
-        return userMapper.updateUser(user)>0;
+    public boolean updateUser(User user) {
+        return userMapper.updateUser(user) > 0;
     }
 
-    public boolean saveOrUpdateAddress(UserAddress userAddress){
-        if (userAddress.getId()>0){
-            return userMapper.updateAddress(userAddress)>0;
-        }else {
-            return userMapper.insertAddress(userAddress)>0;
+    public boolean saveOrUpdateAddress(UserAddress userAddress) {
+        if (userAddress.getIsDefault() == 0) {
+            userMapper.updateDefaultAddress(userAddress.getUserId());
+        }
+        UserAddress pojo = userMapper.userAddress(userAddress.getId());
+        if (Objects.nonNull(pojo)) {
+            return userMapper.updateAddress(userAddress) > 0;
+        } else {
+            return userMapper.insertAddress(userAddress) > 0;
         }
     }
 
-    public List<UserAddress> searchAddressList(long userId){
+    public List<UserAddress> searchAddressList(long userId) {
         return userMapper.searchAddressList(userId);
     }
 
-    public UserAddress userAddress(long id){
+    public UserAddress userAddress(long id) {
         return userMapper.userAddress(id);
     }
 
-    public void deleteAddress(long id){
+    public void deleteAddress(long id) {
         userMapper.deleteAddress(id);
     }
 
-    public int updateDefaultAddress(){
-        return userMapper.updateDefaultAddress();
-    }
-
-    public int register(User user){
-        return userMapper.register(user);
+    public boolean register(User user, String code) {
+        SalesUser salesUser = salesUserService.searchSalesUserCode(code);
+        if (Objects.isNull(salesUser)) {
+            throw new ServiceException("不是有效的业务员");
+        }
+        if (this.updateUser(user)) {
+            SalesUserRelation salesUserRelation = new SalesUserRelation();
+            salesUserRelation.setUserId(user.getId());
+            salesUserRelation.setSalesmanId(salesUser.getId());
+            salesUserRelation.setIsCheck(2);
+            if (salesUserService.insertRelation(salesUserRelation) < 1) {
+                throw new ServiceException("注册审核提交失败");
+            }
+            return true;
+        }
+        return false;
     }
 
 }
