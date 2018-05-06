14207efb6ad4f8e41cdb2ec9817f6be7f23a75ec
282854419@qq.com <282854419@qq.com>
2018-05-06 21:31:40 +0800

    订单数据类型根据数据表字段类型修改，将long改为包装类Long
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/lhiot/mall/wholesale/base/DataMergeUtils.java
    #	src/main/java/com/lhiot/mall/wholesale/order/domain/OrderDetail.java
    #	src/main/java/com/lhiot/mall/wholesale/user/domain/User.java

--- src/main/java/com/lhiot/mall/wholesale/pay/domain/PaymentLog.java
+++ src/main/java/com/lhiot/mall/wholesale/pay/domain/PaymentLog.java
@@ -1,6 +1,7 @@
 package com.lhiot.mall.wholesale.pay.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import lombok.Data;
@@ -15,25 +16,25 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 public class PaymentLog {
     @JsonProperty("id")
-    private long id;
+    private Long id;
 
     @JsonProperty("orderId")
-    private long orderId;
+    private Long orderId;
 
     @JsonProperty("orderCode")
     private String orderCode;
 
     @JsonProperty("userId")
-    private long userId;
+    private Long userId;
 
     @JsonProperty("transactionId")
     private String transactionId;
 
     @JsonProperty("refundFee")
-    private int refundFee;
+    private Integer refundFee;
 
     @JsonProperty("totalFee")
-    private int totalFee;
+    private Integer totalFee;
 
     @JsonProperty("bankType")
     private String bankType;
@@ -42,14 +43,15 @@ public class PaymentLog {
     private String bankName;
 
     @JsonProperty("paymentFrom")
-    private int paymentFrom;
+    private String paymentFrom;
 
     @JsonProperty("paymentStep")
-    private int paymentStep;
+    private String paymentStep;
 
     @JsonProperty("paymentOrderType")
-    private int paymentOrderType;
+    private String paymentOrderType;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     @JsonProperty("paymentTime")
     private Timestamp paymentTime;
 
