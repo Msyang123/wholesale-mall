e716d7ef3ee5908908b0dac8d10d32aebb9343ef
Administrator <Administrator@PC-20180315HULB>
2018-05-07 18:01:36 +0800

    优惠券配置管理

--- src/main/java/com/lhiot/mall/wholesale/coupon/domain/gridparam/CouponGridParam.java
+++ src/main/java/com/lhiot/mall/wholesale/coupon/domain/gridparam/CouponGridParam.java
@@ -22,8 +22,11 @@ public class CouponGridParam extends PageObject {
 	@ApiModelProperty(notes="优惠券状态 no-无效 yes-有效",dataType="String")
 	private String vaild;
 	
-	@ApiModelProperty(notes="优惠券类型  all-全品类 single-单品类",dataType="String")
+	@ApiModelProperty(notes="配置类型  activity-活动送券 artificial-手动发券",dataType="String")
 	private String couponType;
+	
+	@ApiModelProperty(notes="优惠券类型  all-全品类 single-单品类",dataType="String")
+	private String couponTypePlate;
 	
 	@ApiModelProperty(notes="用户电话",dataType="String")
 	private String phone;
