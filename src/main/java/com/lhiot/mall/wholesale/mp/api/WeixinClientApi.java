3beead911ed65b50265a017d970ad94e0795e386
yijun <290828414@qq.com>
2018-05-16 18:42:53 +0800

    提交微信服务处理

--- src/main/java/com/lhiot/mall/wholesale/mp/api/WeixinClientApi.java
+++ src/main/java/com/lhiot/mall/wholesale/mp/api/WeixinClientApi.java
@@ -2,12 +2,15 @@ package com.lhiot.mall.wholesale.mp.api;
 
 import com.lhiot.mall.wholesale.base.MySecurity;
 import com.lhiot.mall.wholesale.mp.api.backmsg.*;
+import com.lhiot.mall.wholesale.user.wechat.PaymentProperties;
+import com.lhiot.mall.wholesale.user.wechat.WeChatUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RMapCache;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,7 +22,6 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @Api(description = "微信公众平台对接接口")
 @Slf4j
@@ -28,9 +30,12 @@ public class WeixinClientApi {
 
     private final RedissonClient redissonClient;
 
+    private final WeChatUtil weChatUtil;
+
     @Autowired
-    public WeixinClientApi(RedissonClient redissonClient) {
+    public WeixinClientApi(RedissonClient redissonClient, PaymentProperties properties) {
         this.redissonClient = redissonClient;
+        this.weChatUtil = new WeChatUtil(properties);
     }
 
 
@@ -63,76 +68,96 @@ public class WeixinClientApi {
 
     @PostMapping("/sign")
     @ApiOperation(value = "微信发送的信息响应")
-    public void postSign(@RequestBody String msg, HttpServletRequest request, HttpServletResponse response) {
-       log.info(msg);
+    public void postSign(@RequestParam("signature") String signature,
+                         @RequestParam("timestamp") String timestamp,
+                         @RequestParam("nonce") String nonce,
+                         HttpServletRequest request, HttpServletResponse response) {
         try{
-            response.setContentType("text/html; charset=utf-8");
-            InputStream is = request.getInputStream();
-            OutputStream os = response.getOutputStream();
-            final DefaultSession session = DefaultSession.newInstance();
-
-            session.addOnHandleMessageListener(new HandleMessageAdapter() {
-                //自动回复消息
-                @Override
-                public void onTextMsg(Msg4Text msg) {
-                    log.info("Msg4Text:",msg);
+            // 重写totring方法，得到三个参数的拼接字符串
+            List<String> list = new ArrayList<String>(3) {
+                private static final long serialVersionUID = 2621444383666420433L;
+                public String toString() {
+                    return this.get(0) + this.get(1) + this.get(2);
                 }
-            });
+            };
+            list.add("shuiguoshule");//正式环境要修改下
+            list.add(timestamp);
+            list.add(nonce);
+            Collections.sort(list);// 排序
+            String tmpStr = new MySecurity().encode(list.toString(),
+                    MySecurity.SHA_1);// SHA-1加密
 
-            // 语音识别消息
-            session.addOnHandleMessageListener(new HandleMessageAdapter() {
+            if (signature.equals(tmpStr)) {
+                response.setContentType("text/html; charset=utf-8");
+                InputStream is = request.getInputStream();
+                OutputStream os = response.getOutputStream();
+                final DefaultSession session = DefaultSession.newInstance();
 
-                public void onVoiceMsg(Msg4Voice msg) {
-                    Msg4Text reMsg = new Msg4Text();
-                    reMsg.setFromUserName(msg.getToUserName());
-                    reMsg.setToUserName(msg.getFromUserName());
-                    reMsg.setCreateTime(msg.getCreateTime());
-                    reMsg.setContent("识别结果: " + msg.getRecognition());
-                    session.callback(reMsg);// 回传消息
-                }
-            });
-
-            // 处理事件
-            session.addOnHandleMessageListener(new HandleMessageAdapter() {
-                public void onEventMsg(Msg4Event msg) {
-
-                    //根据当期接收人自定义消息给予回复
-                    String eventType = msg.getEvent();
-                    if (Msg4Event.SUBSCRIBE.equals(eventType)) {// 订阅
-                        //查找当前接收人设置的订阅消息
-                        log.info("eventType:",eventType);
-
-                    } else if (Msg4Event.UNSUBSCRIBE.equals(eventType)) {// 取消订阅
-                        log.info("取消关注：" + msg.getFromUserName());
-                        RMapCache<String,String> cache=  redissonClient.getMapCache("userToken");
-                        //将access_token(2小时) 缓存清除掉
-                        cache.remove("accessToken"+msg.getFromUserName());
-                        //redis缓存 refresh_token一个月 缓存清除掉
-                        cache.remove("refreshToken"+msg.getFromUserName());
-                        log.info("取消后清除了accessToken：" + cache.get("accessToken"+msg.getFromUserName()));
-                        log.info("refreshToken：" + cache.get("refreshToken"+msg.getFromUserName()));
-                    } else if (Msg4Event.CLICK.equals(eventType)) {// 点击事件
-                        //查找当前接收人
-                        log.info("eventType:",eventType);
-                    }else if(Msg4Event.SUBSCRIBE.equals(eventType)) {
-                        log.info("关注公众号：" + msg.getFromUserName());
+                session.addOnHandleMessageListener(new HandleMessageAdapter() {
+                    //自动回复消息
+                    @Override
+                    public void onTextMsg(Msg4Text msg) {
+                        log.info("Msg4Text:", msg);
                     }
-                }
-            });
+                });
 
-            // 处理地理位置
-            session.addOnHandleMessageListener(new HandleMessageAdapter() {
-                public void onLocationMsg(Msg4Location msg) {
-                    System.out.println("收到地理位置消息：");
-                    System.out.println("X:" + msg.getLocation_X());
-                    System.out.println("Y:" + msg.getLocation_Y());
-                    System.out.println("Scale:" + msg.getScale());
-                }
+                // 语音识别消息
+                session.addOnHandleMessageListener(new HandleMessageAdapter() {
 
-            });
+                    public void onVoiceMsg(Msg4Voice msg) {
+                        Msg4Text reMsg = new Msg4Text();
+                        reMsg.setFromUserName(msg.getToUserName());
+                        reMsg.setToUserName(msg.getFromUserName());
+                        reMsg.setCreateTime(msg.getCreateTime());
+                        reMsg.setContent("识别结果: " + msg.getRecognition());
+                        session.callback(reMsg);// 回传消息
+                    }
+                });
 
-            session.process(is, os);// 处理微信消息
-            session.close();// 关闭Session
+                // 处理事件
+                session.addOnHandleMessageListener(new HandleMessageAdapter() {
+                    public void onEventMsg(Msg4Event msg) {
+
+                        //根据当期接收人自定义消息给予回复
+                        String eventType = msg.getEvent();
+                        if (Msg4Event.SUBSCRIBE.equals(eventType)) {// 订阅
+                            //查找当前接收人设置的订阅消息
+                            log.info("eventType:", eventType);
+
+                        } else if (Msg4Event.UNSUBSCRIBE.equals(eventType)) {// 取消订阅
+                            log.info("取消关注：" + msg.getFromUserName());
+                            RMapCache<String, String> cache = redissonClient.getMapCache("userToken");
+                            //将access_token(2小时) 缓存清除掉
+                            cache.remove("accessToken" + msg.getFromUserName());
+                            //redis缓存 refresh_token一个月 缓存清除掉
+                            cache.remove("refreshToken" + msg.getFromUserName());
+                            log.info("取消后清除了accessToken：" + cache.get("accessToken" + msg.getFromUserName()));
+                            log.info("refreshToken：" + cache.get("refreshToken" + msg.getFromUserName()));
+                        } else if (Msg4Event.CLICK.equals(eventType)) {// 点击事件
+                            //查找当前接收人
+                            log.info("eventType:", eventType);
+                        } else if (Msg4Event.SUBSCRIBE.equals(eventType)) {
+                            log.info("关注公众号：" + msg.getFromUserName());
+                        }
+                    }
+                });
+
+                // 处理地理位置
+                session.addOnHandleMessageListener(new HandleMessageAdapter() {
+                    public void onLocationMsg(Msg4Location msg) {
+                        System.out.println("收到地理位置消息：");
+                        System.out.println("X:" + msg.getLocation_X());
+                        System.out.println("Y:" + msg.getLocation_Y());
+                        System.out.println("Scale:" + msg.getScale());
+                    }
+
+                });
+
+                session.process(is, os);// 处理微信消息
+                session.close();// 关闭Session
+            }else {
+                log.error("微信用户消息回调签名错误"+signature+timestamp+nonce);
+            }
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -140,5 +165,13 @@ public class WeixinClientApi {
         }
     }
 
-
+    @PostMapping("/rebulidWexinMenu")
+    @ApiOperation(value = "微信生成自定义菜单")
+    public ResponseEntity rebulidWexinMenu(@RequestBody String menuContent) throws IOException {
+        return ResponseEntity.ok(
+                weChatUtil.httpsRequest("https://api.weixin.qq.com/cgi-bin/menu/create?access_token="+
+                                weChatUtil.getToken().getAccessToken(),
+                "POST",menuContent)
+        );
+    }
 }
--- src/main/java/com/lhiot/mall/wholesale/order/api/OrderApi.java
+++ src/main/java/com/lhiot/mall/wholesale/order/api/OrderApi.java
@@ -14,20 +14,19 @@ import com.lhiot.mall.wholesale.goods.service.GoodsPriceRegionService;
 import com.lhiot.mall.wholesale.goods.service.GoodsService;
 import com.lhiot.mall.wholesale.invoice.domain.Invoice;
 import com.lhiot.mall.wholesale.order.domain.Distribution;
-import com.lhiot.mall.wholesale.order.domain.gridparam.OrderGridParam;
-import com.lhiot.mall.wholesale.setting.domain.ParamConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import com.lhiot.mall.wholesale.order.domain.OrderDetail;
 import com.lhiot.mall.wholesale.order.domain.OrderGoods;
 import com.lhiot.mall.wholesale.order.domain.OrderGridResult;
+import com.lhiot.mall.wholesale.order.domain.gridparam.OrderGridParam;
 import com.lhiot.mall.wholesale.order.service.OrderService;
+import com.lhiot.mall.wholesale.setting.domain.ParamConfig;
 import com.lhiot.mall.wholesale.setting.service.SettingService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
 import java.beans.IntrospectionException;
