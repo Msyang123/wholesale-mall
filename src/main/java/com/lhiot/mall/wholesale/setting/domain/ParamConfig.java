ae5fe5650012d13720274e182bebb44ddf3cc312
Administrator <Administrator@PC-20180315HULB>
2018-05-20 21:33:13 +0800

    系统参数设置

--- src/main/java/com/lhiot/mall/wholesale/setting/domain/ParamConfig.java
+++ src/main/java/com/lhiot/mall/wholesale/setting/domain/ParamConfig.java
@@ -33,5 +33,8 @@ public class ParamConfig {
     @ApiModelProperty(notes="后台管理系统，使用字段",dataType="String")
     private String showType;
     
+    @ApiModelProperty(notes="后台管理系统,使用字段,分类编号",dataType="String")
+    private String paramCategoryCode;
+    
     private String notes;
 }
