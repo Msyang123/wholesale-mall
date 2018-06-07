735db0d4c5ea21bd4e0c44041683fc3ac1f69d53
Administrator <Administrator@PC-20180315HULB>
2018-06-07 18:08:47 +0800

    放开余额支付注释

--- src/main/java/com/lhiot/mall/wholesale/pay/api/CurrencyPayApi.java
+++ src/main/java/com/lhiot/mall/wholesale/pay/api/CurrencyPayApi.java
@@ -62,13 +62,11 @@ public class CurrencyPayApi {
     @PutMapping("/orderpay/{orderCode}")
     @ApiOperation(value = "余额支付订单", response = String.class)
     public ResponseEntity orderPay(@PathVariable("orderCode") String orderCode) throws Exception {
-		System.out.println("begin....");
-		System.out.println("end....");
-/*        OrderDetail orderDetail = orderService.searchOrder(orderCode);
+        OrderDetail orderDetail = orderService.searchOrder(orderCode);
         if (Objects.isNull(orderDetail)){
             return ResponseEntity.badRequest().body("没有该订单信息");
-        }*/
-/*        if(!"unpaid".equals(orderDetail.getOrderStatus())){
+        }
+        if(!"unpaid".equals(orderDetail.getOrderStatus())){
             return ResponseEntity.badRequest().body("订单状态异常，请检查订单状态");
         }
 
@@ -76,8 +74,8 @@ public class CurrencyPayApi {
         String payResult=payService.currencyPay(orderDetail);
         if(StringUtils.isEmpty(payResult)){
             return ResponseEntity.ok(orderDetail);
-        }*/
-        return null;//ResponseEntity.badRequest().body(payResult);
+        }
+        return ResponseEntity.badRequest().body(payResult);
     }
 
     @PutMapping("/debtorderpay/{orderDebtCode}")
